2ced00a74e2990e2d8626667a90d73c55ba62945
Goodnews Adewole <goodnews.adewole@example.com>
2023-02-21 07:30:39 +0100

    added env

--- todolist.js
+++ todolist.js
@@ -124,6 +124,6 @@ app.get("/about", function(req, res){
     res.render("about")
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT||3000, function(){
     console.log("Hello World")
 }) 
